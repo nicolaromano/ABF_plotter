b7a4ae3fc5687ba601c2d1f67d66ac460b90e4eb
Nicola Romano <romano.nicola@gmail.com>
2024-08-08 15:24:22 +0100

    example usage

--- example.R
+++ example.R
@@ -16,6 +16,8 @@ plot_ABF_data(recording, sweep=1, channel=1, time_from=-20, time_to=10)
 # Plot data from 500 to 510 seconds
 plot_ABF_data(recording, sweep=1, channel=1, time_from=500, time_to=510)
 # As above, but highlight the x range from 501 to 503 seconds
-plot_ABF_data(recording, sweep=1, channel=1, time_from=500, time_to=510, highlight_x_range = c(501, 503))
+plot_ABF_data(recording, sweep=1, channel=1, time_from=500, time_to=510, 
+    highlight_x_range = c(501, 503))
 # Change the highlight color to red
-plot_ABF_data(recording, sweep=1, channel=1, time_from=800, time_to=1200, highlight_y_range = c(-60, -40), highlight_color = "red")
+plot_ABF_data(recording, sweep=1, channel=1, time_from=0, time_to=2000, 
+    highlight_y_range = c(-60, -40), highlight_color = "red")
